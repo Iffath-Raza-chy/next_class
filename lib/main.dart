28b9367a8da1e2f768971691208956addf579a4a
Iffath-Raza-chy <iffathraza@gmail.com>
2021-11-03 21:14:02 +0600

    Major Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:next_class/pages/home_page.dart';
+import 'package:flutter/services.dart';
+import 'package:next_class/screens/welcome_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +11,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GoSchool UI',
+      theme: ThemeData(
+        primaryColor: Color(0xFF202328),
+        accentColor: Color(0xFF63CF93),
+        backgroundColor: Color(0xFF12171D),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: WelcomeScreen(),
     );
   }
 }
